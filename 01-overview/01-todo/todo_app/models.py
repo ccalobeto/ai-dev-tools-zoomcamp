8b0552e1e36d3d9beb25ed946d48108cb8a572b3
ccalobeto <calobeto.leon@gmail.com>
2025-11-24 06:42:05 -0500

    Add Todo model and register in admin; update template directory path

--- 01-overview/01-todo/todo_app/models.py
+++ 01-overview/01-todo/todo_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
+    is_resolved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
