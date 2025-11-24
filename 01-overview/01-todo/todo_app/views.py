7d3b12b338cdb682d4ff3aeebf61cc45c41c7ec8
ccalobeto <calobeto.leon@gmail.com>
2025-11-24 06:50:18 -0500

    Implement CRUD functionality for Todo app with forms, views, and templates

--- 01-overview/01-todo/todo_app/views.py
+++ 01-overview/01-todo/todo_app/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, 'todo_app/todo_list.html', {'todos': todos})
+
+def todo_create(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todo_app/todo_form.html', {'form': form})
+
+def todo_edit(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'todo_app/todo_form.html', {'form': form})
+
+def todo_delete(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('todo_list')
+    return render(request, 'todo_app/todo_confirm_delete.html', {'todo': todo})
+
+def todo_resolve(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.is_resolved = True
+    todo.save()
+    return redirect('todo_list')
